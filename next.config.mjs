6cf72680ba2ca83e0b8b3e0ca6a1f784fba5e532
Candela Colombo <135880086+ccolombo95@users.noreply.github.com>
2024-09-22 21:37:41 -0300

    Cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  devIndicators: {
+    autoPrerender: false,
+  },
+};
 
 export default nextConfig;
